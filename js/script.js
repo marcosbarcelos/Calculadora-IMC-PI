600e03245340c50fa79990b4700135ae8f7412ce
Marcos Barcelos <marcosbarcelosdev@gmail.com>
2022-03-04 11:25:39 -0300

    code review

--- js/script.js
+++ js/script.js
@@ -1,46 +1,50 @@
-const calcular = document.getElementById("calcular");
+const buttonCalcular = document.getElementById('calcular');
 
-function imc() {
-  const altura = document.getElementById("altura").value;
-  const peso = document.getElementById("peso").value;
-  const resultado = document.getElementById("resultado");
-  const select = document.getElementById("sexo");
+function calcular() {
+  const altura = document.getElementById('altura').value;
+  const peso = document.getElementById('peso').value;
+  const resultado = document.getElementById('resultado');
+  const select = document.getElementById('sexo');
   const selectValue = select.options[select.selectedIndex].value;
 
-  if (altura !== "" && peso !== "" && selectValue !== "") {
-    document.getElementById("container").style.display = "none";
-    document.getElementById("container_resultado").style.display = "flex";
+  if (altura !== '' && peso !== '' && selectValue !== '') {
+    document.getElementById('container').style.display = 'none';
+    document.getElementById('container_resultado').style.display = 'flex';
 
+    // fórmula que calcula o IMC
     const valorIMC = (peso / (altura * altura)).toFixed(1);
 
-    let classificacaoIMC = "";
+    let classificacaoIMC = '';
 
     if (valorIMC < 18.5) {
-      classificacaoIMC = "peso abaixo do normal";
-    }else if (valorIMC < 25) {
-      classificacaoIMC = " a classificação excelente, Parabéns!!";
-    }else if (valorIMC < 30) {
-      classificacaoIMC = "sobrepeso";
-    }else if (valorIMC < 35) {
-      classificacaoIMC = "Obesidade Grau I";
-    }else if (valorIMC < 40) {
-      classificacaoIMC = "Obesidade Grau II";
-    }else if (valorIMC >= 40) {
-      classificacaoIMC = "Obesidade Grau III ou Mórbida, ATENÇÃO!!";
+      classificacaoIMC = 'peso abaixo do normal';
+    } else if (valorIMC < 25) {
+      classificacaoIMC = 'a classificação excelente';
+    } else if (valorIMC < 30) {
+      classificacaoIMC = 'sobrepeso';
+    } else if (valorIMC < 35) {
+      classificacaoIMC = 'Obesidade Grau I';
+    } else if (valorIMC < 40) {
+      classificacaoIMC = 'Obesidade Grau II';
+    } else if (valorIMC >= 40) {
+      classificacaoIMC = 'Obesidade Mórbida';
     }
 
-    let valorPI = ""; 
+    let valorPI = '';
 
-    if (selectValue == "f") {
+    if (selectValue == 'feminino') {
+      //fórmula de peso ideal para sexo feminino
       valorPI = (21 * (altura * altura)).toFixed(1);
-    }else {
+    } else {
+      //fórmula de peso ideal para sexo masculino
       valorPI = (22 * (altura * altura)).toFixed(1);
     }
 
     resultado.textContent = `Seu IMC é ${valorIMC}, você está com ${classificacaoIMC} e seu peso ideal é ${valorPI}Kg`;
-  }else {
-    alert("Preencha os campos solicitados.");
+
+  } else {
+    alert('Preencha os campos solicitados.');
   }
 }
 
-calcular.addEventListener("click", imc);
+buttonCalcular.addEventListener('click', calcular);
